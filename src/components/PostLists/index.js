57187e695df9fed080fcd0297be8e2652f3cb627
Kaung Zaw Han <kaungzawhan.zotefamily@gmail.com>
2026-05-07 18:56:49 +0630

    build : create modal box and show

--- src/components/PostLists/index.js
+++ src/components/PostLists/index.js
@@ -2,14 +2,16 @@ import React from "react";
 import "./index.css";
 
 export default function index({posts}) {
-  console.log(posts);
+
   return (
     <div className="post-list">
       {posts.map((post) => (
         <div key={post.id} className="post-item">
-          {post.title}
+          <h2>{post.title}</h2>
+          <p>{post.description}</p>
         </div>
       ))}
     </div>
   );
 }
+  
