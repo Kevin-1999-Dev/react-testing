e85c5350c2f6c4c91d2bc0264a7249582f76149a
Kaung Zaw Han <kaungzawhan.zotefamily@gmail.com>
2026-05-07 19:33:49 +0630

    modify : posts design

--- src/components/PostLists/index.js
+++ src/components/PostLists/index.js
@@ -1,17 +1,18 @@
 import React from "react";
 import "./index.css";
 
-export default function index({posts}) {
-
+export default function index({ posts }) {
   return (
     <div className="post-list">
       {posts.map((post) => (
         <div key={post.id} className="post-item">
           <h2>{post.title}</h2>
           <p>{post.description}</p>
+          <div className="post-item-footer">
+            <span className="post-delete-btn">Delete</span>
+          </div>
         </div>
       ))}
     </div>
   );
 }
-  
