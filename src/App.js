3d02f61706405e16b3be50fe99c892c3e4cf48fa
Kaung Zaw Han <kaungzawhan.zotefamily@gmail.com>
2026-05-08 11:58:49 +0630

    build : navbar and posts

--- src/App.js
+++ src/App.js
@@ -1,46 +1,32 @@
 import "./App.css";
 import { useState } from "react";
+import NavBar from "./components/NavBar/index.js";
+import PostLists from "./components/PostLists/index.js";
 
 function App() {
-  let [name, setName] = useState("Hello");
 
   let [posts, setPosts] = useState([
     {
       id: 1,
       title: "Post 1",
+      description: "This is the description for Post 1",
     },
     {
       id: 2,
       title: "Post 2",
+      description: "This is the description for Post 2",
     },
     {
       id: 3,
       title: "Post 3",
+      description: "This is the description for Post 3",
     },
   ]);
 
-  let changeName = () => {
-    setName((prevState) => (prevState = "Kaung"));
-  };
-
-  let deletePost = (id) => {
-    setPosts((prevState)=> prevState.filter((post)=>post.id !== id))
-  }
-
   return (
     <div className="App">
-      <h1>{name}</h1>
-      <button onClick={changeName}>Change Name</button>
-      <hr />
-      <h2>Posts</h2>
-      <ul>
-        {!!posts.length && posts.map((post) =>
-          <li key={post.id}>
-            {post.title}
-            <button onClick={()=>deletePost(post.id)}>Delete</button>
-          </li>)}
-          {posts.length === 0 && <p>No posts available</p>}
-      </ul>
+      <NavBar />
+      <PostLists posts={posts} />
     </div>
   );
 }
---
+++ src/components/NavBar/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./index.css";
+
+export default function index() {
+  return (
+    <nav>
+      <div className="nav-container">
+        <h1>Logo</h1>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
